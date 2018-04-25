004477b10b033b69291b302ca04f9d7ecf0cfe32
guenole lorho <lorho.guenole@gmail.com>
2018-04-25 20:13:19 +0100

    file directory try n°15 (set public as root)

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const bodyParser = require('body-parser')
 const app = express();
 
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
