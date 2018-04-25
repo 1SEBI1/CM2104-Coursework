8235ca75b3693865b393dd5278606fe88e964042
guenole lorho <lorho.guenole@gmail.com>
2018-04-25 19:00:01 +0100

    setting up server(express, mongodb) + template ejs (account, home and saved pages)

---
+++ server.js
@@ -0,0 +1,35 @@
+const MongoClient = require('mongodb').MongoClient; //npm install mongodb@2.2.32
+const url = "mongodb://localhost:27017/profiles";
+const express = require('express'); //npm install express
+const session = require('express-session'); //npm install express-session
+const bodyParser = require('body-parser'); //npm install body-parser
+const app = express();
+
+//this tells express we are using sesssions. These are variables that only belong to one user of the site at a time.
+app.use(session({ secret: 'example' }));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.set('view engine', 'ejs');
+
+var db;
+
+MongoClient.connect(url, function(err, database) {
+  if (err) throw err;
+  db = database;
+  app.listen(8080);
+  console.log('listening on 8080');
+});
+
+
+app.get('/', function(req, res) {
+  res.render('pages/home');
+});
+app.get('/saved', function(req, res) {
+  res.render('pages/saved');
+});
+app.get('/account', function(req, res) {
+  res.render('pages/account');
+});
