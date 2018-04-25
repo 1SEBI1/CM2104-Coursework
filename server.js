296bf958655aaa31785939e578677e1e68f7f63a
guenole lorho <lorho.guenole@gmail.com>
2018-04-25 19:15:55 +0100

    sever setup tryn°2

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-const MongoClient = require('mongodb').MongoClient; //npm install mongodb@2.2.32
-const url = "mongodb://localhost:27017/profiles";
-const express = require('express'); //npm install express
-const session = require('express-session'); //npm install express-session
-const bodyParser = require('body-parser'); //npm install body-parser
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/star_wars_quotes";
+const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
-//this tells express we are using sesssions. These are variables that only belong to one user of the site at a time.
-app.use(session({ secret: 'example' }));
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}))
-
+app.use(bodyParser.urlencoded({extended: true}))
+// set the view engine to ejs
 app.set('view engine', 'ejs');
 
 var db;
@@ -20,8 +15,9 @@ MongoClient.connect(url, function(err, database) {
   if (err) throw err;
   db = database;
   app.listen(8080);
-  console.log('listening on 8080');
+  console.log('listening');
 });
+
 
 
 app.get('/', function(req, res) {
