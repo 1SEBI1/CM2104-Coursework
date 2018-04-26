46a1d6bc3a2be3cbf9fe06d4867231b3a4752eab
guenole lorho <lorho.guenole@gmail.com>
2018-04-26 14:54:39 +0100

    login db test

--- server.js
+++ server.js
@@ -44,19 +44,12 @@ app.post('/dologin', function(req, res) {
   var pword = req.body.password;
 
   db.collection('users').find().toArray(function(err, result) {
-    if(!result){
-      console.log("res : "+ result);
-      //res.redirect('/login')}
-
     if (err) throw err;
-    //the result of the query is sent to the users page as the "users" array
-    console.log("results : " + JSON.stringify(result));
-    //res.redirect('/')
-
-    }
+    console.log(result);
+    res.redirect('/login')
 
   });
-/*
+  /*
   db.collection('users').findOne({"login.username":uname}, function(err, result) {
     if (err) throw err;//if there is an error, throw the error
     //if there is no result, redirect the user back to the login system as that username must not exist
