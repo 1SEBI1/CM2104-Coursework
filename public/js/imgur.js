132c958c4c9efceeb2f877a6d7098bd47e115510
1SEBI1 <sebi9d@gmail.com>
2018-04-27 20:17:03 +0100

    final changes

--- public/js/imgur.js
+++ public/js/imgur.js
@@ -1,29 +1,12 @@
+//funtion that gets the value from the searchbar
 $(function(){
   $('#searchbar').on("change", function(){
-  searchbar($(this).context.value);
-  searchquery = $('#searchbar').val();
+    searchbar($(this).context.value);
+    searchquery = $('#searchbar').val();
   });
-/*  $("#tempobject-").click(function(){
-      /*  #tempobject.substr(0, 11)
-        console.log(JSON.stringify(tempobject))
-        console.log("click");
-    });*/
-
-    $('img').click(function() {
-      console.log("click");
-        if($(this).hasClass('selected')) {
-            $(this).removeClass('selected');
-            console.log("selected");
-        }
-        else {
-            $(this).addClass('selected');
-            console.log("deselected");
-        }
-    });
-
-
 });
 
+//function that displays the Imgur images on the jars
 function arraysToJars(arrays) {
   var stopcrash =0;
   var count =0;
@@ -33,15 +16,14 @@ function arraysToJars(arrays) {
   var imgcount =0;
   var htmlstring ="";
   htmlstring +="<div class='row'>";
-for(var i = 0; i < 4; i++) {
-  htmlstring +="<div class='column'>";
+  for(var i = 0; i < 4; i++) {
+    htmlstring +="<div class='column'>";
 
-  for(var y = 0; y < 3; ) {
-    var imglink;
-
-    //console.log("temp"+count+" = " +tempobject[count]);
-    if(typeof arrays.data[imgcount] === 'undefined') {break; } //if image data array undefined break
-    if(typeof arrays.data[imgcount].images === 'undefined') {
+    for(var y = 0; y < 3; ) {
+      var imglink;
+      //if image data array undefined break
+      if(typeof arrays.data[imgcount] === 'undefined') {break; }
+      if(typeof arrays.data[imgcount].images === 'undefined') {
         jars[imgcount] = arrays.data[imgcount].link;
         imglink = jars[imgcount];
         if((imglink.includes(".mp4"))==false){
@@ -55,105 +37,100 @@ for(var i = 0; i < 4; i++) {
           if (stopcrash >50) { break; }
         }
         imgcount +=1;
-    }else{
-      jars[imgcount] = arrays.data[imgcount].images[imagesno].link;
-        imglink = jars[imgcount];
-      if(arrays.data[imgcount].images.length == imagesno+1){
-        imagesno = 0;
-
-        if((imglink.includes(".mp4"))==false){
-          tempobject[count] = {link:imglink, query:searchquery};
-          htmlstring +="<img src='" + imglink+ "' alt='cookie1' id=tempobject-"+count+" class='cookieimg' style='width:100%'>";
-          //console.log("array : " + arrays.data[imgcount].images[imagesno].title);
-          count +=1;
-          y+=1;
-        }else{//skip display for mp4
-          stopcrash +=1;
-          if (stopcrash >50) { break; }
-        }
-        imgcount +=1;
       }else{
-        imagesno +=1;
-        if((imglink.includes(".mp4"))==false){
-          tempobject[count] = {link:imglink, query:searchquery};
-          htmlstring +="<img src='" + imglink+ "' alt='cookie1' id=tempobject-"+count+" class='cookieimg' style='width:100%'>";
-          count +=1;
-          y+=1;
-        }else{//skip display for mp4
-          stopcrash +=1;
-          if (stopcrash >50) { break; }
-        }
-      }
+        jars[imgcount] = arrays.data[imgcount].images[imagesno].link;
+        imglink = jars[imgcount];
+        if(arrays.data[imgcount].images.length == imagesno+1){
+          imagesno = 0;
 
-    }
-    if (count >50) { break; }
-    //console.log("count : " + count);
+          if((imglink.includes(".mp4"))==false){
+            tempobject[count] = {link:imglink, query:searchquery};
+            htmlstring +="<img src='" + imglink+ "' alt='cookie1' id=tempobject-"+count+" class='cookieimg' style='width:100%'>";
+            count +=1;
+            y+=1;
+          }else{//skip display for mp4
+            stopcrash +=1;
+            if (stopcrash >50) { break; }
+          }
+          imgcount +=1;
+        }else{
+          imagesno +=1;
+          if((imglink.includes(".mp4"))==false){
+            tempobject[count] = {link:imglink, query:searchquery};
+            htmlstring +="<img src='" + imglink+ "' alt='cookie1' id=tempobject-"+count+" class='cookieimg' style='width:100%'>";
+            count +=1;
+            y+=1;
+          }else{//skip display for mp4
+            stopcrash +=1;
+            if (stopcrash >50) { break; }
+          }
+        }
+
+      }
+      if (count >50) { break; }
     }
     htmlstring +="</div>"
 
   }
   htmlstring +="</div>"
-  //console.log("imgtemp length" + tempobject.length);
+  //convert to html and send to the imgurContainer
   $('#imgurContainer').html(htmlstring);
-  
+
 }
 
-
+//function that displays the flickr images
 function flickrToJars(arrays) {
-    var htmlstring ="";
-    var image = "";
-   
-        htmlstring +="<div class='row'>";
-        for(var i = 0,x = 0; i < 4, x < 12; i++) {
-            htmlstring +="<div class='column'>";
-        for(var j = 0; j < 3; j++) {
-            image = arrays.items[x].media.m;
-            htmlstring +="<img src='" + image + "' alt='cookie1' class='cookieimg' style='width:100%'>";
-            x++;
+  var htmlstring ="";
+  var image = "";
+
+  htmlstring +="<div class='row'>";
+  for(var i = 0,x = 0; i < 4, x < 12; i++) {
+    htmlstring +="<div class='column'>";
+    for(var j = 0; j < 3; j++) {
+      image = arrays.items[x].media.m;
+      htmlstring +="<img src='" + image + "' alt='cookie1' class='cookieimg' style='width:100%'>";
+      x++;
     }
-            htmlstring +="</div>"
+    htmlstring +="</div>"
   }
-        htmlstring +="</div>"
+  htmlstring +="</div>"
+  //convert to html and send to the Container
   $('#redditContainer').html(htmlstring);
 }
 
-
-
-
-
-
-
-
 /**
- * Search function
- * @param query the searching string
- */
+* Search function
+* @param query the searching string
+*/
 function searchbar(query) {
+  //imgur search querry
+  $.ajax({
+    url: 'https://api.imgur.com/3/gallery/search?q='+ query,
+    headers: {
+      'Authorization' : 'Client-ID 2984ccc930c582a'
+    },
+    type: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+      //call display function
+      console.log(arraysToJars(data));
+    },
+    error: function(response) {
+      console.log(response);
+    }
+  });
 
-	$.ajax({
-		url: 'https://api.imgur.com/3/gallery/search?q='+ query,
-		headers: {
-			'Authorization' : 'Client-ID 2984ccc930c582a'
-		},
-		type: 'GET',
-		dataType: 'json',
-		success: function(data) {
-			console.log(data);
-            console.log(arraysToJars(data));
-		},
-		error: function(response) {
-			console.log(response);
-		}
-	});
+  //flickr search querry
+  $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
+  {
+    tags: query,
+    tagmode: "any",
+    format: "json"
+  },
+  function(data) {
+    //call display function
+    console.log(flickrToJars(data));
+  });
 
-        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",
-        {
-          tags: query,
-          tagmode: "any",
-          format: "json"
-        },
-        function(data) {
-          console.log(flickrToJars(data));
-        });
-     
 }
--- server.js
+++ server.js
@@ -21,8 +21,8 @@ MongoClient.connect(url, function(err, database) {
 });
 
 app.get('/', function(req, res) {
-    if(!req.session.loggedin){res.redirect('/login');return;}
-    res.render('pages/home');
+  if(!req.session.loggedin){res.redirect('/login');return;}
+  res.render('pages/home');
 });
 
 app.get('/saved', function(req, res) {
@@ -52,85 +52,81 @@ app.post('/dologin', function(req, res) {
     //if there is no result, redirect the user back to the login system as that username must not exist
     if(!result){
       res.redirect('/login');return}
-    //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
-    if(result.login.password == pword){
-      req.session.loggedin = true;
-      req.session.username = uname;
-      res.redirect('/')}
-    //otherwise send them back to login
-    else{
-      res.redirect('/login')}
-  });
-});
+      //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
+      if(result.login.password == pword){
+        req.session.loggedin = true;
+        req.session.username = uname;
+        res.redirect('/')}
+        //otherwise send them back to login
+        else{
+          res.redirect('/login')}
+        });
+      });
 
-app.get('/logout', function(req, res) {
-  req.session.loggedin = false;
-  req.session.destroy();
-  res.redirect('/');
-});
+      //logout funtion
+      app.get('/logout', function(req, res) {
+        req.session.loggedin = false;
+        req.session.destroy();
+        res.redirect('/');
+      });
 
-
-app.post('/createaccount', function(req, res) {
-var newuserdata = {
-"name":{"first":req.body.first,"last":req.body.last},
-"login":{"username":req.body.username,"password":req.body.password},
-"email":req.body.email,
-"dob":req.body.dob,"registered":Date()}
-
-
-//once created we just run the data string against the database and all our new data will be saved/
-  db.collection('users').save(newuserdata, function(err, result) {
-    if (err) throw err;
-    console.log('saved to database')
-    //when complete redirect to the index
-    res.redirect('/')
-  })
-});
-
-app.get('/edit', function(req,res) {
-  if(!req.session.loggedin){res.redirect('/login');return;}
-
-  db.collection('users').findOne({"login.username":req.session.username}, function(err, result) {
-    if (err) throw err;//if there is an error, throw the error
-    //if(!result){res.redirect('/login');return}
-
-    res.render('pages/edit',{
-      user: result
-    })
-  });
-});
+      app.post('/createaccount', function(req, res) {
+        var newuserdata = {
+          "name":{"first":req.body.first,"last":req.body.last},
+          "login":{"username":req.body.username,"password":req.body.password},
+          "email":req.body.email,
+          "dob":req.body.dob,"registered":Date()}
 
 
+          //once created we just run the data string against the database and all our new data will be saved/
+          db.collection('users').save(newuserdata, function(err, result) {
+            if (err) throw err;
+            console.log('saved to database')
+            //when complete redirect to the index
+            res.redirect('/')
+          })
+        });
 
+        app.get('/edit', function(req,res) {
+          if(!req.session.loggedin){res.redirect('/login');return;}
 
+          db.collection('users').findOne({"login.username":req.session.username}, function(err, result) {
+            if (err) throw err;//if there is an error, throw the error
+            //if(!result){res.redirect('/login');return}
 
-app.post('/doedit', function(req, res) {
-  //check we are logged in
-  //we create the data string from the form components that have been passed in
-var newvalues = {$set:{
-"name":{"first":req.body.newfirst,"last":req.body.newlast},
-"email":req.body.newemail,
-"login":{"username":req.body.newusername},
-"dob":req.body.newdob}};
-  console.log("first :" +req.body.newfirst);
+            res.render('pages/edit',{
+              user: result
+            })
+          });
+        });
 
-//once created we just run the data string against the database and all our new data will be saved/
-  db.collection('users').updateOne({"login.username":req.session.username}, newvalues, function(err, result) {
-    if (err) throw err;
-    console.log('updated to database');
-    //when complete redirect to the index
-    res.redirect('/');
-  });
-});
+        app.post('/doedit', function(req, res) {
+          //check we are logged in
+          //we create the data string from the form components that have been passed in
+          var newvalues = {$set:{
+            "name":{"first":req.body.newfirst,"last":req.body.newlast},
+            "email":req.body.newemail,
+            "login":{"username":req.body.newusername},
+            "dob":req.body.newdob}};
+            console.log("first :" +req.body.newfirst);
 
-app.get('/delete', function(req, res) {
-  //check we are logged in.
-  if(!req.session.loggedin){res.redirect('/login');return;}
+            //once created we just run the data string against the database and all our new data will be saved/
+            db.collection('users').updateOne({"login.username":req.session.username}, newvalues, function(err, result) {
+              if (err) throw err;
+              console.log('updated to database');
+              //when complete redirect to the index
+              res.redirect('/');
+            });
+          });
 
-  db.collection('users').deleteOne({"login.username":req.session.username}, function(err, result) {
-    if (err) throw err;
-    //when complete redirect to the index
-    res.redirect('/logout');
-  });
-});
+          app.get('/delete', function(req, res) {
+            //check we are logged in.
+            if(!req.session.loggedin){res.redirect('/login');return;}
 
+            //delete curret logged in user
+            db.collection('users').deleteOne({"login.username":req.session.username}, function(err, result) {
+              if (err) throw err;
+              //when complete redirect to the index
+              res.redirect('/logout');
+            });
+          });
